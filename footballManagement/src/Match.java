96e3847ec1bee0f603620660c38a056c15009535
thmvu <151448141+thmvu@users.noreply.github.com>
2025-04-29 03:10:53 +0000

    object Match

---
+++ footballManagement/src/Match.java
@@ -0,0 +1,14 @@
+public class Match{
+
+    // variables
+
+    String matchName;
+    String team;
+
+    //Methods
+
+    public String getTeam(String Team){
+        team = Team;
+        return team;
+    }
+}
