9e9002086ecb21ee783a3b9aa768039fa8a378a6
thmvu <151448141+thmvu@users.noreply.github.com>
2025-05-10 14:40:51 +0700

    commit

---
+++ StorageManagement/test/Concho.java
@@ -0,0 +1,17 @@
+public class Concho {
+    public String Ten_loai_cho;
+    public String mau_long;
+    public int Tuoi;
+    
+    public void nhapTuoi(int x){
+        this.Tuoi =x;
+    }
+    public int hienthituoi(){
+        return this.Tuoi;
+    }
+
+    public void sua(){
+        System.out.println("Gau Gau");
+
+    }
+}
