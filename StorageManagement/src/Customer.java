4a93c0bf4449336d4d31c8391db60ff0967ec567
thmvu <151448141+thmvu@users.noreply.github.com>
2025-05-13 10:58:03 +0700

    sua customer

--- StorageManagement/src/Customer.java
+++ StorageManagement/src/Customer.java
@@ -1,20 +1,21 @@
 
 public class Customer extends User         //tính kế thừa 
 {
-    public void signUp(int userId, String name, String gender, String birthDate, String phoneNumber,        //tính đa hình
-                String email, String address, String password, String userType) 
+    Customer(int userId, String name, String gender, String birthDate, String phoneNumber)        //tính đa hình
     {
         this.userId = userId;
         this.name = name;
         this.gender = gender;
         this.birthDate = birthDate;
         this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.address = address;
-        this.password = password;
-        this.userType = "Khách hàng";
+        //this.email = email;
+        //this.address = address;
+        //this.password = password;
+        //this.userType = "Khách hàng";
     }
-    
+    public void showInfo(){
+        System.out.println("ID: "+userId + ",\n Ten : "+name + ",\n Gioi Tinh: " + gender + ",\n Ngay Sinh: " + birthDate + ",\n So Dien Thoai: " + phoneNumber);
+    }
     public void buy(String productName)     //mua sản phẩm
     {
         System.out.println(name + " đã mua sản phẩm: " + productName);
